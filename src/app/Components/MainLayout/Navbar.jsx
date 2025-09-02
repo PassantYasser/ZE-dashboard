b02d8c656a9ba656f853ba3222c5583758fce2a0
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-02 14:59:42 +0300

    feat: Implement language selection in Navbar and Sidebar components

--- src/app/Components/MainLayout/Navbar.jsx
+++ src/app/Components/MainLayout/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import i18n from "../../../language/i18n";
+
 
 function Navbar() {
+    const [open, setOpen] = useState(true);
+  
+    const handleLangChange = (e) => {
+    const newLang = e.target.value;
+    i18n.changeLanguage(newLang);
+    document.documentElement.setAttribute("dir", newLang === "ar" ? "rtl" : "ltr");
+  };
   return (
     <>
-    <header className="h-14 bg-green-600 text-white flex items-center px-4">
+    <header className="h-14 bg-green-600 text-white flex items-center justify-between px-4">
       <h1 className="text-lg font-bold">Zetime</h1>
+
+        {/* Language Selector مثبت في الأسفل */}
+    <div className="">
+      <select
+        onChange={handleLangChange}
+        value={i18n.language}
+        className={`bg-amber-300 h-10 text-center cursor-pointer rounded-2xl outline-none ${open ? "w-32" : "w-12"}`}
+      >
+        <option value="en">{open ? "English" : "En"}</option>
+        <option value="ar">{open ? "العربية" : "ar"}</option>
+      </select>
+    </div>
     </header>
     </>
   )
--- src/app/Components/MainLayout/Sidebar.jsx
+++ src/app/Components/MainLayout/Sidebar.jsx
@@ -9,25 +9,21 @@ function Sidebar() {
   const [open, setOpen] = useState(true);
   const { t } = useTranslation();
 
-    const [activeIndex, setActiveIndex] = useState(null); // track clicked li
+  const [activeIndex, setActiveIndex] = useState(null); // track clicked li
   const [currentPath, setCurrentPath] = useState(""); // store path/name clicked
 
-  const handleLangChange = (e) => {
-    const newLang = e.target.value;
-    i18n.changeLanguage(newLang);
-    document.documentElement.setAttribute("dir", newLang === "ar" ? "rtl" : "ltr");
-  };
+
 
   return (
     <aside 
-      className={`border-l border-[#E3E8EF] transition-all p-4 duration-300 h-screen relative 
+      className={`border-x border-[#E3E8EF] transition-all p-4 duration-200 h-screen relative 
         ${open ? "w-70" : "w-18"}`}
     >
 
       {/* Logo open and close */}
       <button
         onClick={() => setOpen(!open)}
-        className="w-full flex justify-center mt-10 mb-14 "
+        className="w-full flex justify-center mt-10 mb-14 cursor-pointer "
       >
         {open ?(
           <div className='flex gap-2 items-center'>
@@ -44,155 +40,149 @@ function Sidebar() {
 
       {/* Navigation */}
       <nav className="">
-        <ul>
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 0 ? "bg-red-500" : ""}`}
+        <ul className=''>
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 0 ? "bg-[#C69815] text-[#fff] " : ""}`}
               onClick={() => { setActiveIndex(0); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/dashboard.svg" alt="" />
+                <img src="/images/icons/dashboard.svg" alt="" className={activeIndex === 0 ? "invert" : ""}/>
                 <p>{t('dashboard')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/dashboard.svg" alt="" />
+                <img src="/images/icons/dashboard.svg" alt="" className={activeIndex === 0 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 1 ? "bg-red-500" : ""}`}
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 1 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(1); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/Requests.svg" alt="" />
+                <img src="/images/icons/Requests.svg" alt="" className={activeIndex === 1 ? "invert" : ""}/>
                 <p>{t('Requests')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/Requests.svg" alt="" />
+                <img src="/images/icons/Requests.svg" alt="" className={activeIndex === 1 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 2 ? "bg-red-500" : ""}`}
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 2 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(2); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/workers.svg" alt="" />
+                <img src="/images/icons/workers.svg" alt="" className={activeIndex === 2 ? "invert" : ""}/>
                 <p>{t('workers')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/workers.svg" alt="" />
+                <img src="/images/icons/workers.svg" alt="" className={activeIndex === 2 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 3 ? "bg-red-500" : ""}`}
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 3 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(3); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/Services.svg" alt="" />
+                <img src="/images/icons/Services.svg" alt="" className={activeIndex === 3 ? "invert" : ""}/>
                 <p>{t('Services')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/Services.svg" alt="" />
+                <img src="/images/icons/Services.svg" alt="" className={activeIndex === 3 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 4 ? "bg-red-500" : ""}`}
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 4 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(4); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/conversations.svg" alt="" />
+                <img src="/images/icons/conversations.svg" alt="" className={activeIndex === 4 ? "invert" : ""}/>
                 <p>{t('conversations')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/conversations.svg" alt="" />
+                <img src="/images/icons/conversations.svg" alt="" className={activeIndex === 4 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 5 ? "bg-red-500" : ""}`}
+          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 5 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(5); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/Finance.svg" alt="" />
+                <img src="/images/icons/Finance.svg" alt="" className={activeIndex === 5 ? "invert" : ""}/>
                 <p>{t('Finance')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/Finance.svg" alt="" />
+                <img src="/images/icons/Finance.svg" alt="" className={activeIndex === 5 ? "invert" : ""} />
               </div>
             )}
           </li>
 
-          <li className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 6 ? "bg-red-500" : ""}`}
+          <li className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 6 ? "bg-[#C69815] text-[#fff]" : ""}`}
             onClick={() => { setActiveIndex(6); setCurrentPath('dashboard'); }}>
             {open?(
             //open 
               <div className='flex gap-4 items-center'>
-                <img src="/images/icons/dashboard.svg" alt="" />
+                <img src="/images/icons/dashboard.svg" alt="" className={activeIndex === 6 ? "invert" : ""} />
                 <p>{t('technical support')}</p>
               </div>
             ):(
               <div className='flex justify-center items-center'>
-                <img src="/images/icons/dashboard.svg" alt="" />
+                <img src="/images/icons/dashboard.svg" alt="" className={activeIndex === 6 ? "invert" : ""}/>
               </div>
             )}
           </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 7 ? "bg-red-500" : ""}`}
-            onClick={() => { setActiveIndex(7); setCurrentPath('dashboard'); }}>
-            {open?(
-            //open 
-              <div className='flex gap-4 items-center'>
-                <img src="/images/icons/settings.svg" alt="" />
-                <p>{t('Settings')}</p>
-              </div>
-            ):(
-              <div className='flex justify-center items-center'>
-                <img src="/images/icons/settings.svg" alt="" />
-              </div>
-            )}
-          </li>
 
-          <li  className={`cursor-pointer py-4 px-2 rounded ${activeIndex === 8 ? "bg-red-500" : ""}`}
-            onClick={() => { setActiveIndex(8); setCurrentPath('dashboard'); }}>
-            {open?(
-            //open 
-              <div className='flex gap-4 items-center'>
-                <img src="/images/icons/signout.svg" alt="" />
-                <p className='text-[#D92D20]'>{t('Sign out')}</p>
-              </div>
-            ):(
-              <div className='flex justify-center items-center'>
-                <img src="/images/icons/signout.svg" alt="" />
-              </div>
-            )}
-          </li>
+        <div  >
+            <li  className={`cursor-pointer py-4 px-2 rounded  mt-auto mb-2  ${activeIndex === 7 ? "bg-[#C69815] text-[#fff]" : ""}`}
+              onClick={() => { setActiveIndex(7); setCurrentPath('dashboard'); }}>
+              {open?(
+              //open 
+                <div className='flex gap-4 items-center'>
+                  <img src="/images/icons/settings.svg" alt=""className={activeIndex === 7 ? "invert" : ""} />
+                  <p>{t('Settings')}</p>
+                </div>
+              ):(
+                <div className='flex justify-center items-center'>
+                  <img src="/images/icons/settings.svg" alt="" className={activeIndex === 7 ? "invert" : ""}/>
+                </div>
+              )}
+            </li>
+          
+            <li  className={`cursor-pointer py-4 px-2 rounded  mt-auto mb-2  ${activeIndex === 8 ? "bg-[#C69815] text-[#fff]" : ""}`}
+              onClick={() => { setActiveIndex(8); setCurrentPath('dashboard'); }}>
+              {open?(
+              //open 
+                <div className='flex gap-4 items-center'>
+                  <img src="/images/icons/signout.svg" alt="" />
+                  <p className='text-[#D92D20]'>{t('Sign out')}</p>
+                </div>
+              ):(
+                <div className='flex justify-center items-center'>
+                  <img src="/images/icons/signout.svg" alt="" />
+                </div>
+              )}
+            </li>
+        </div>
+
         </ul>
       </nav>
 
-      {/* Language Selector مثبت في الأسفل */}
-    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
-      <select
-        onChange={handleLangChange}
-        value={i18n.language}
-        className={`bg-amber-300 h-10 text-center cursor-pointer rounded-2xl outline-none ${open ? "w-32" : "w-12"}`}
-      >
-        <option value="en">{open ? "English" : "En"}</option>
-        <option value="ar">{open ? "العربية" : "ar"}</option>
-      </select>
-    </div>
+    
 
     </aside>
   );
