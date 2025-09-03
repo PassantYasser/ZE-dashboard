71581d89f85b2fd805ed27da9dac669e85fef14e
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-03 12:27:39 +0300

    feat: Enhance Navbar with language support and add welcome messages in Arabic and English

--- src/app/Components/MainLayout/Navbar.jsx
+++ src/app/Components/MainLayout/Navbar.jsx
@@ -1,24 +1,31 @@
 "use client";
-
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import i18n from "../../../language/i18n";
+import { useTranslation } from 'react-i18next';
 
 
 function Navbar() {
     const [open, setOpen] = useState(true);
+    const { t } = useTranslation();
   
+    // Language change handler
     const handleLangChange = (e) => {
-    const newLang = e.target.value;
-    i18n.changeLanguage(newLang);
-    document.documentElement.setAttribute("dir", newLang === "ar" ? "rtl" : "ltr");
-  };
+      const newLang = e.target.value;
+      i18n.changeLanguage(newLang);
+      document.documentElement.setAttribute("dir", newLang === "ar" ? "rtl" : "ltr");
+    };
+
+
   return (
     <>
-    <header className="h-14 bg-green-600 text-white flex items-center justify-between px-4">
-      <h1 className="text-lg font-bold">Zetime</h1>
-
-        {/* Language Selector مثبت في الأسفل */}
+    <header className="h-20 bg-[#fff] border-b border-[#E3E8EF] flex items-center justify-between px-4">
+      <div>
+        <p>{t('Welcome back!')} </p>
+        <p>{t('Lets check your update today')}</p>
+      </div>
     <div className="">
+
+      {/* Language Selector */}
       <select
         onChange={handleLangChange}
         value={i18n.language}
@@ -27,6 +34,7 @@ function Navbar() {
         <option value="en">{open ? "English" : "En"}</option>
         <option value="ar">{open ? "العربية" : "ar"}</option>
       </select>
+
     </div>
     </header>
     </>
