305373ac37890d44c7f6005dee667f54e25cb0b9
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-09 11:14:23 +0300

    feat: enhance LoginPage layout with improved responsiveness and styling; adjust global font sizes for better readability

--- src/app/Auth/Login/page.jsx
+++ src/app/Auth/Login/page.jsx
@@ -1,75 +1,147 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import i18n from "../../../language/i18n";
-import { useTranslation } from 'react-i18next';
-import Link from 'next/link';
+import { useTranslation } from "react-i18next";
+import Link from "next/link";
 function LoginPage() {
-  
-      const { t } = useTranslation();
-  
+  const { t } = useTranslation();
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <>
-    <div className='p-8 lg1340:flex justify-between gap-8 h-screen '>
-      <section className='w-full'>
-          <div className=' mt-50.5 mb-25 flex flex-col items-center   rounded-[10px]'>
-            <p className='text-[#9E7A11] text-[32px] font-semibold mb-6'>{t('Welcome back!')}</p>
-            <p className='text-[#656565] text-2xl font-normal'>{t('Log in to access your account.')} </p>
+      <div className="p-8 lg1340:flex justify-between gap-8 h-screen ">
+        <section className="w-full">
+
+          {/* 📱Tablet screen only */}
+          <div className="lg1340:hidden flex justify-center gap-1 my-20">
+            <img src="/images/LogoText.svg" alt="" />
+            <img src="/images/Logo.svg" alt="" />
           </div>
-          <form className='w-full flex flex-col gap-6'>
-            <div className='flex flex-col gap-2'>
-              <label className='text-[#364152] text-base font-normal' htmlFor="email">{t('Email')}/{t('phone number')}</label>
-              <input className='w-full h-15 px-3 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A] placeholder:text-sm' type="email" name="email" id="email" placeholder={t('Email')} />   
-            </div>
-            <div className='flex flex-col gap-2'>
-              <label className='text-[#364152] text-base font-normal' htmlFor="password">{t('password')}</label>
-              <input className='w-full h-15 px-3 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A] placeholder:text-sm' type="password" name="password" id="password" placeholder={t('password')} />
-              <Link href='/Auth/Login/ForgetPassword' className='flex justify-end text-[#9E7A11] text-base font-normal'>{t('Forgot your password?')}</Link>
-            </div>
-            <button className='w-full h-14 bg-[#DDA918] text-white text-lg font-semibold rounded-[3px] mt-4 mb-12'>
-              {t('Log in')}
-            </button>
-            <p className='flex justify-center gap-1.5'>
-              <span className='text-[#697586] text-lg font-normal'>{t('Dont have an account?')}</span>
-              <span className='text-[#9E7A11] text-lg font-medium'>{t('Create an account')}</span>
+
+          {/* title  */}
+          <div className=" lg1340:mt-50.5 mb-17.5 lg1340:mb-25 flex flex-col lg1340:items-center   rounded-[10px]">
+            <p className="text-[#9E7A11] text-[32px] font-semibold mb-6">
+              {t("Welcome back!")}
             </p>
-          </form>   
+            <p className="text-[#656565] text-2xl font-normal">
+              {t("Log in to access your account.")}{" "}
+            </p>
+          </div>
 
-      </section>
-      
+          <form className="w-full flex flex-col gap-6">
 
-  
-        
-        <section className='w-full hidden lg1340:block  rounded-[10px]'
-          style={{ background: "linear-gradient(180deg, #DDA918 48.1%, #9D7810 99.85%)" }}
+            {/* email form */}
+            <div className="flex flex-col gap-3">
+              <label
+                className="text-[#364152] fontSizeA font-normal"
+                htmlFor="email"
+              >
+                {t("Email")}/{t("phone number")}
+              </label>
+              <input
+                className="w-full h-15 p-3 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A] placeholder:text-sm"
+                type="email"
+                name="email"
+                id="email"
+                placeholder={t("Email")}
+              />
+            </div>
+
+            {/* password form */}
+            <div className="flex flex-col gap-3">
+              <label
+                className="text-[#364152] fontSizeA font-normal"
+                htmlFor="password"
+              >
+                {t("password")}
+              </label>
+
+              <div className="relative">
+                <input
+                  className="w-full h-15 p-3 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A] placeholder:text-sm"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  placeholder={t("password")}
+                />
+                <span
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+                >
+                  {showPassword ?
+                    (
+                      <img src="/images/icons/eyeClose.svg" alt="" />
+                    )
+                    :(
+                      <img src="/images/icons/eyeOpen.svg" alt="" />
+                    )
+                  }
+                </span>
+              </div>
+
+              {/* btn of forget password */}
+              <Link
+                href="/Auth/Login/ForgetPassword"
+                className="flex justify-end text-[#9E7A11] fontSizeA font-normal"
+              >
+                {t("Forgot your password?")}
+              </Link>
+
+            </div>
+
+            <button className="w-full h-14 bg-[#DDA918] text-white text-base font-medium rounded-[3px] mt-4 mb-12">
+              {t("Log in")}
+            </button>
+            
+            {/*btn to open signup */}
+            <p className="flex justify-center gap-1.5">
+              <span className="text-[#697586] text-lg font-normal">
+                {t("Dont have an account?")}
+              </span>
+              <span className="text-[#9E7A11] text-lg font-medium">
+                {t("Create an account")}
+              </span>
+            </p>
+
+          </form>
+          
+        </section>
+
+        {/* 💻 desktop screen only */}
+        <section
+          className="w-full hidden lg1340:block  rounded-[10px]"
+          style={{
+            background:
+              "linear-gradient(180deg, #DDA918 48.1%, #9D7810 99.85%)",
+          }}
         >
-          <p className='flex justify-end ml-4'>
+          <p className="flex justify-end ml-4">
             <img src="/images/AuthLogUP.png" alt="" />
           </p>
-        
+
           <div className="mt-4.5 w-[70%] mx-auto flex flex-col gap-4 text-center text-white ">
             <p className="text-3xl font-bold">
               {t("Join the ZETIME Partner Network")}
             </p>
             <p className="text-lg font-normal leading-9">
-              {t("Register now and get the tools you need to reach a wider customer base and achieve your goals efficiently.")}
+              {t(
+                "Register now and get the tools you need to reach a wider customer base and achieve your goals efficiently."
+              )}
             </p>
-            <p className='mt-20'>
+            <p className="mt-20">
               <img src="/images/AuthLogMiddle.svg" alt="" />
             </p>
           </div>
-  
-          <p className='fixed bottom-8 mr-4 '>
+
+          <p className="fixed bottom-8 mr-4 ">
             <img src="/images/AuthLogDown.svg" alt="" />
           </p>
-    
         </section>
-  
+
+
       </div>
-
-
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
