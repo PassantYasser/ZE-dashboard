c5a25d3cd865105da3b86601eecd5268095362a4
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-14 15:47:35 +0300

    enhance owner information form responsiveness and spacing

--- src/app/Auth/Signup/Company/OwnerInformation/page.jsx
+++ src/app/Auth/Signup/Company/OwnerInformation/page.jsx
@@ -10,56 +10,66 @@ function OwnerInformationPage({ onNext, onPrev, currentStep, steps }) {
   const { t } = useTranslation();
   return (
     <>
-      <form className="flex flex-col mt-8 ">
-        <label className="text-[#364152] text-base font-normal mb-3">
-          {t("First Name")}
-        </label>
-        <input 
-          type="text" 
-          className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
-          placeholder={t('Enter first name')}
-        />
+      <form className="grid grid-cols-2 gap-6 lg1:grid-cols-1 lg1:gap-0  mt-8 ">
+        <div className="flex flex-col">
+          <label className="text-[#364152] text-base font-normal mb-3">
+            {t("First Name")}
+          </label>
+          <input 
+            type="text" 
+            className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
+            placeholder={t('Enter first name')}
+          />
+        </div>
 
-        <label className="text-[#364152] text-base font-normal mb-3">
-          {t("Last Name")}/ {t("Family Name")}{" "}
-        </label>
-        <input 
-          type="text" 
-          className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
-          placeholder={t('Enter last name/family name')}
-        />
+        <div className="flex flex-col">
+          <label className="text-[#364152] text-base font-normal mb-3">
+            {t("Last Name")}/ {t("Family Name")}{" "}
+          </label>
+          <input 
+            type="text" 
+            className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
+            placeholder={t('Enter last name/family name')}
+          />
+        </div>
 
-        <label className="text-[#364152] text-base font-normal mb-3">
-          {t("Email")} 
-        </label>
-        <input 
-          type="text" 
-          className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
-          placeholder={t('Enter your email')}
-        />
+        <div className="flex flex-col">
+          <label className="text-[#364152] text-base font-normal mb-3">
+            {t("Email")} 
+          </label>
+          <input 
+            type="text" 
+            className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
+            placeholder={t('Enter your email')}
+          />
+        </div>
 
 
-        <label className="text-[#364152] text-base font-normal mb-3">
-          {t("National ID number")} 
-        </label>
-        <input 
-          type="text" 
-          className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
-          placeholder={t('Enter your national ID number')}
-        />
+        <div className="flex flex-col">
+          <label className="text-[#364152] text-base font-normal mb-3">
+            {t("National ID number")} 
+          </label>
+          <input 
+            type="text" 
+            className=" h-15 p-3 mb-6 border border-[#C8C8C8] rounded-[3px] placeholder-[#9A9A9A]"
+            placeholder={t('Enter your national ID number')}
+          />
+        </div>
 
-        <label className="text-[#364152] text-base font-normal mb-3 block">
-          {t("Mobile number")}
-        </label>
-        <div className="relative">
-        <PhoneInput
-          country={'sa'} 
-          placeholder="000000000"
-          containerClass="!w-full"
-          inputClass="!w-full !h-[60px] !border !border-[#C8C8C8] !rounded-[3px] !pl-24 !text-left !text-[#364152] placeholder-[#9A9A9A]"
-          buttonClass="!absolute !left-0 !top-0 !h-full !px-3 !flex !items-center !gap-2 !bg-transparent !border-0"
-          dropdownClass="!text-[#364152] !border "
-        />
+        <div className="flex flex-col col-span-2 lg1:col-span-1">
+          <label className="text-[#364152] text-base font-normal mb-3 block">
+            {t("Mobile number")}
+          </label>
+          <div className="relative">
+          <PhoneInput
+            country={'sa'} 
+            placeholder="000000000"
+            containerClass="!w-full"
+            inputClass="!w-full !h-[60px] !border !border-[#C8C8C8] !rounded-[3px] !pl-24 !text-left !text-[#364152] placeholder-[#9A9A9A]"
+            buttonClass="!absolute !left-0 !top-0 !h-full !px-3 !flex !items-center !gap-2 !bg-transparent !border-0"
+            dropdownClass="!text-[#364152] !border "
+          />
+          </div>
         </div>
             
       </form>
