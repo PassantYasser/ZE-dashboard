af0d38a4bacf6ce9a7ad7c83909864a7cd8833cf
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-17 14:52:53 +0300

    enhance layout with overflow fixes and code cleanup

--- src/app/Components/MainLayout/MainLayout.jsx
+++ src/app/Components/MainLayout/MainLayout.jsx
@@ -1,20 +1,49 @@
+// "use client";
+// import React, { useState } from 'react'
+// import Sidebar from './Sidebar'
+// import Navbar from './Navbar'
+
+// function MainLayout({children}) {
+//     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+//   return (
+//   <div className="flex h-screen">
+//     <Sidebar  isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}  />
+//     <div className="flex flex-1 flex-col">
+//       <Navbar onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+//       <main className="lg1:pt-8 pt-10 px-6">{children}</main>
+//     </div>
+//   </div>  
+//   )
+// }
+
+// export default MainLayout
+
 "use client";
 import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 
-function MainLayout({children}) {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+function MainLayout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-  <div className="flex h-screen">
-    <Sidebar  isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}  />
-    <div className="flex flex-1 flex-col">
-      <Navbar onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
-      <main className="lg1:pt-8 pt-10 px-6">{children}</main>
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+
+      {/* Content */}
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Navbar onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+        <main className="flex-1 overflow-y-auto lg1:pt-8 pt-10 px-6">
+          {children}
+        </main>
+      </div>
     </div>
-  </div>  
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
