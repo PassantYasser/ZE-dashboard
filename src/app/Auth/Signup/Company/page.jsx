e7d11417f942abe108fe776f4f9548f06d3ea703
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-09-29 10:08:40 +0300

    feat(Signup): integrate SecondSection component for improved signup experience

--- src/app/Auth/Signup/Company/page.jsx
+++ src/app/Auth/Signup/Company/page.jsx
@@ -5,6 +5,7 @@ import CompanyInformationPage from './CompanyInformation/page';
 import OwnerInformationPage from './OwnerInformation/page';
 import SetPasswordPage from './SetPassword/page';
 import VerificationPage from './Verification/page';
+import SecondSection from '@/app/Components/login/SecondSection';
 
 function CompanyPage() {
   const {t}= useTranslation()
@@ -115,34 +116,8 @@ const [currentStep, setCurrentStep] = useState(0);
         </section>
 
 
-        <section className="w-full hidden lg1:block rounded-[10px]"
-          style={{
-            background:
-              "linear-gradient(180deg, #DDA918 48.1%, #9D7810 99.85%)",
-          }}
-        >
-          <p className="flex justify-end ml-4">
-            <img src="/images/AuthLogUP.png" alt="" className='w-29 h-49' />
-          </p>
-
-          <div className="mt-4.5 w-[70%] mx-auto flex flex-col gap-4 text-center text-white ">
-            <p className="text-3xl font-bold">
-              {t("Join the ZETIME Partner Network")}
-            </p>
-            <p className="text-lg font-normal leading-9">
-              {t(
-                "Register now and get the tools you need to reach a wider customer base and achieve your goals efficiently."
-              )}
-            </p>
-            <p className="mt-20">
-              <img src="/images/AuthLogMiddle.svg" alt="" />
-            </p>
-          </div>
-
-          <p className="  ">
-            <img src="/images/AuthLogDown.svg" alt="" className='w-29 h-49' />
-          </p>
-        </section>
+        {/* second section */}
+      <SecondSection />
       </div>
 
     </>
--- src/app/Auth/Signup/page.jsx
+++ src/app/Auth/Signup/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import SecondSection from '@/app/Components/login/SecondSection';
 import Link from 'next/link';
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -40,34 +41,8 @@ function SignuPage() {
           </div>
         </section>
 
-          <section className="w-full hidden lg1:block rounded-[10px]"
-          style={{
-            background:
-              "linear-gradient(180deg, #DDA918 48.1%, #9D7810 99.85%)",
-          }}
-        >
-          <p className="flex justify-end ml-4">
-            <img src="/images/AuthLogUP.png" alt="" className='w-29 h-49' />
-          </p>
-
-          <div className="mt-4.5 w-[70%] mx-auto flex flex-col gap-4 text-center text-white ">
-            <p className="text-3xl font-bold">
-              {t("Join the ZETIME Partner Network")}
-            </p>
-            <p className="text-lg font-normal leading-9">
-              {t(
-                "Register now and get the tools you need to reach a wider customer base and achieve your goals efficiently."
-              )}
-            </p>
-            <p className="mt-20">
-              <img src="/images/AuthLogMiddle.svg" alt="" />
-            </p>
-          </div>
-
-          <p className="  ">
-            <img src="/images/AuthLogDown.svg" alt="" className='w-29 h-49' />
-          </p>
-        </section>
+          {/* second section */}
+      <SecondSection />
       </div>
 
     </>
