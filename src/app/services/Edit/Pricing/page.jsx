b09015a246b8314ac66b68ef07642cc5fc36cda2
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-10-08 17:24:15 +0300

    fix: add max height and overflow to dropdowns for better usability

--- src/app/services/Edit/Pricing/page.jsx
+++ src/app/services/Edit/Pricing/page.jsx
@@ -77,7 +77,7 @@ function PricingPage({handlePrev , handleGoBack}) {
               </span>
             </div>
             {open1 && (
-              <ul className="absolute left-0 right-0 border border-[#C8C8C8] bg-white rounded-[3px] shadow-md z-10">
+              <ul className="absolute left-0 right-0 border border-[#C8C8C8] bg-white rounded-[3px] shadow-md z-10  max-h-48 overflow-y-auto">
                 {options.map((option, index) => (
                   <li
                     key={index}
