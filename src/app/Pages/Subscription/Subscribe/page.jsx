7d0a2fc4e1f0a29b8debb6a64a25324d9f840e42
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-10-27 15:29:19 +0300

    feat(subscription): add subscribe page and update navigation link
    - Create new SubscribePage component under Subscription directory
    - Update 'Subscribe now' link in AcceptAccountPage to point to new subscription page

---
+++ src/app/Pages/Subscription/Subscribe/page.jsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/app/Components/MainLayout/MainLayout'
+import React from 'react'
+
+function SubscribePage() {
+  return (
+    <MainLayout>SubscribePage</MainLayout>
+  )
+}
+
+export default SubscribePage
--- src/app/Pages/dashboard/TemporaryDashboard/AcceptAccount/page.jsx
+++ src/app/Pages/dashboard/TemporaryDashboard/AcceptAccount/page.jsx
@@ -33,11 +33,11 @@ function AcceptAccountPage() {
           <p className='text-[#656565] text-xl font-normal'>{t('To add your services and benefit from the platform s full benefits, subscribe to one of our packages.')}</p>
         </section>
 
-      <section className='mb-5 flex justify-center mt-8'>
-        <Link href='/Pages/Subscription' className='bg-[var(--color-primary)] rounded-[3px] py-2.5 px-4 w-62.5 h-13.5 cursor-pointer flex items-center justify-center'>
-          <span className='text-[#fff] text-base font-medium'>{t('Subscribe now')}</span>
-        </Link>
-      </section>
+        <section className='mb-5 flex justify-center mt-8'>
+          <Link href='/Pages/Subscription/Subscribe' className='bg-[var(--color-primary)] rounded-[3px] py-2.5 px-4 w-62.5 h-13.5 cursor-pointer flex items-center justify-center'>
+            <span className='text-[#fff] text-base font-medium'>{t('Subscribe now')}</span>
+          </Link>
+        </section>
       </div>
 
 
