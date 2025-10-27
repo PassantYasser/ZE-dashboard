44845933e805f9f7c30e6f60a47d53f6538ca747
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-10-27 16:29:18 +0300

    feat(subscription): add subscription page content and translations
    - Add new translations for subscription-related content in both English and Arabic
    - Implement subscription page with "Subscribe now" button linking to subscription form
    - Include useTranslation hook for internationalization support

--- src/app/Pages/Subscription/page.jsx
+++ src/app/Pages/Subscription/page.jsx
@@ -1,9 +1,19 @@
+"use client"
 import MainLayout from '@/app/Components/MainLayout/MainLayout'
+import Link from 'next/link'
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 function SubscriptionPage() {
+  const {t} = useTranslation(); 
   return (
-    <MainLayout>SubscriptionPage</MainLayout>
+    <MainLayout>
+          <section className='mb-5 flex justify-center mt-8'>
+          <Link href='/Pages/Subscription/Subscribe' className='bg-[var(--color-primary)] rounded-[3px] py-2.5 px-4 w-62.5 h-13.5 cursor-pointer flex items-center justify-center'>
+            <span className='text-[#fff] text-base font-medium'>{t('Subscribe now')}</span>
+          </Link>
+        </section>
+    </MainLayout>
   )
 }
 
