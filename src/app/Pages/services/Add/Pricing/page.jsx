88e879d125717cdd6d3335b9c9836ee668036ef4
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-11-24 10:53:00 +0200

    feat(services): add pricing information notice to service form
    Add a notice box to inform users that the entered amount is only what they receive from the customer, without additional expenses. This improves transparency in the pricing section of the service form.

--- src/app/Pages/services/Add/Pricing/page.jsx
+++ src/app/Pages/services/Add/Pricing/page.jsx
@@ -178,30 +178,38 @@ const [selectedValue2, setSelectedValue2] = useState("");
           </div>
         )}
       </form>
-        <div className="my-12 flex gap-3">
-        <button 
-          onClick={handlePrev} 
-          className="border w-48 h-13.5 py-2.5 px-4 rounded-[3px] border-[#C69815] text-[#C69815] text-base font-medium cursor-pointer"
-        >
-          {t('the previous')}
-        </button>
-        {/* <button
-          onClick={handleGoBack} 
-          className="border w-58 h-13.5 py-2.5 px-4 rounded-[3px] bg-[#C69815] text-[#fff] text-base font-medium cursor-pointer"
-        >
-            {t('save')}
-        </button> */}
-        <button
-          onClick={handleSubmit}
-          disabled={loadingDetails}
-          className={`border w-58 h-13.5 py-2.5 px-4 rounded-[3px] ${
-            loadingDetails ? "bg-gray-400 cursor-not-allowed" : "bg-[#C69815] cursor-pointer"
-          } text-[#fff] text-base font-medium`}
-        >
-          {loadingDetails ? t("Saving...") : t("save")}
-        </button>
 
-        </div>
+      <div className=" flex gap-4 border border-[#FEC84B] bg-[#FFFCF5] rounded-[3px] shadow p-3 mt-6 w-[70%]">
+        <img src="/images/icons/i.svg" alt="" className="w-6 h-6"/>
+        <p className="text-[##202939] text-base font-normal">
+          {t('The amount entered is only what you receive from the customer, without additional expenses or equipment transfer.')}
+        </p>
+      </div>
+
+      <div className="my-12 flex gap-3">
+      <button 
+        onClick={handlePrev} 
+        className="border w-48 h-13.5 py-2.5 px-4 rounded-[3px] border-[#C69815] text-[#C69815] text-base font-medium cursor-pointer"
+      >
+        {t('the previous')}
+      </button>
+      {/* <button
+        onClick={handleGoBack} 
+        className="border w-58 h-13.5 py-2.5 px-4 rounded-[3px] bg-[#C69815] text-[#fff] text-base font-medium cursor-pointer"
+      >
+          {t('save')}
+      </button> */}
+      <button
+        onClick={handleSubmit}
+        disabled={loadingDetails}
+        className={`border w-58 h-13.5 py-2.5 px-4 rounded-[3px] ${
+          loadingDetails ? "bg-gray-400 cursor-not-allowed" : "bg-[#C69815] cursor-pointer"
+        } text-[#fff] text-base font-medium`}
+      >
+        {loadingDetails ? t("Saving...") : t("save")}
+      </button>
+
+      </div>
   
     </>
     
