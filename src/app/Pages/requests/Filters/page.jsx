98fb473893549ad7560800d910d8c6d849c86723
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-11-30 10:20:07 +0200

    feat(requests): enhance FiltersPage with city, service, and status dropdowns; add localization for service selection

--- src/app/Pages/requests/Filters/page.jsx
+++ src/app/Pages/requests/Filters/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
 import { useTranslation } from "react-i18next";
 
@@ -19,13 +19,63 @@ function FiltersPage({ open, handleClose }) {
     );
   };
 
+
+
+    
+    // City (1)
+    // =========================
+    const [open1, setOpen1] = useState(false);
+    const [selected1, setSelected1] = useState(null);
+    const [searchValue1, setSearchValue1] = useState("");
+    const dropdownRef1 = useRef(null);
+    const optionCity = ['gg','hhhh','iiii','jjjj','kkkk','llll','mmmm','nnnn','oooo','pppp'];
+  
+
+    //service (2)
+    // =========================
+    const [open2, setOpen2] = useState(false);
+    const [selected2, setSelected2] = useState(null);
+    const [searchValue2, setSearchValue2] = useState("");
+    const dropdownRef2 = useRef(null);
+    const optionservice = ['gg','hhhh','iiii','jjjj','kkkk','llll','mmmm','nnnn','oooo','pppp'];
+
+    // service (3)
+// =========================
+const [open3, setOpen3] = useState(false);
+const [selected3, setSelected3] = useState(null);
+const [searchValue3, setSearchValue3] = useState("");
+const dropdownRef3 = useRef(null);
+const optionStatus = [
+  'gg',
+  'hhhh',
+  'iiii',
+  'jjjj',
+  'kkkk',
+  'llll',
+  'mmmm',
+  'nnnn',
+  'oooo',
+  'pppp'
+];
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef1.current && !dropdownRef1.current.contains(event.target)) setOpen1(false);
+      if (dropdownRef2.current && !dropdownRef2.current.contains(event.target)) setOpen2(false);
+      if (dropdownRef3.current && !dropdownRef3.current.contains(event.target)) setOpen3(false);   
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+
   return (
     <Dialog
       open={open}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
-      PaperProps={{ className: "ServicePage-dialog" }}
+      PaperProps={{ className: "rerquest-dialog" }}
     >
       <section className="flex justify-between px-6 mt-6">
         <button
@@ -50,13 +100,192 @@ function FiltersPage({ open, handleClose }) {
       <span className="border-[0.5px] border-[#E3E8EF]" />
 
       <section className="p-6">
-        
-        <div>
-          
+        <div className=" grid grid-cols-2 gap-4 mb-4">
+          {/* ==========City ========== */}
+          <div className="flex flex-col">
+            <label className="text-[#364152] text-base font-normal mb-3">
+              {t("City")}
+            </label>
+
+            <div className="relative w-full" ref={dropdownRef1}>
+              <div
+                className="relative flex items-center border border-[#C8C8C8] rounded-[3px] cursor-pointer"
+                onClick={() => setOpen1(!open1)}
+              >
+                <input
+                  type="text"
+                  placeholder={t("Select City")}
+                  value={selected1 || searchValue1}   
+                  onChange={(e) => {
+                    setSearchValue1(e.target.value);
+                    setOpen1(true);
+                    setSelected1(null);
+                  }}
+                  className="h-15 p-3 w-full text-[#364152] focus:outline-none"
+                />
+
+                <span className="absolute left-3 pointer-events-none">
+                  {open1 ? (
+                    <img src="/images/icons/ArrowUp.svg" alt="up" />
+                  ) : (
+                    <img src="/images/icons/ArrowDown.svg" alt="down" />
+                  )}
+                </span>
+              </div>
+
+              {open1 && (
+                <ul className="absolute left-0 right-0 border border-[#C8C8C8] bg-white rounded-[3px] shadow-md z-10 max-h-48 overflow-y-auto">
+                  {optionCity
+                    .filter((opt) =>
+                      opt.toLowerCase().includes(searchValue1.toLowerCase())
+                    )
+                    .map((opt) => (
+                      <li
+                        key={opt}
+                        onClick={() => {
+                          setSelected1(opt);
+                          setOpen1(false);
+                          setSearchValue1("");
+                        }}
+                        className="p-3 hover:bg-[#F5F5F5] cursor-pointer"
+                      >
+                        {opt}
+                      </li>
+                    ))}
+                </ul>
+              )}
+
+            </div>
+          </div>
+
+
+          {/* ==========service ========== */}
+          <div className="flex flex-col">
+            <label className="text-[#364152] text-base font-normal mb-3">
+              {t("Service")}
+            </label>
+
+            <div className="relative w-full" ref={dropdownRef2}>
+              <div
+                className="relative flex items-center border border-[#C8C8C8] rounded-[3px] cursor-pointer"
+                onClick={() => setOpen2(!open2)}
+              >
+                <input
+                  type="text"
+                  placeholder={t("Choose the service")}
+                  value={selected2 || searchValue2}   
+                  onChange={(e) => {
+                    setSearchValue2(e.target.value);
+                    setOpen2(true);
+                    setSelected2(null);
+                  }}
+                  className="h-15 p-3 w-full text-[#364152] focus:outline-none"
+                />
+
+                <span className="absolute left-3 pointer-events-none">
+                  {open2 ? (
+                    <img src="/images/icons/ArrowUp.svg" alt="up" />
+                  ) : (
+                    <img src="/images/icons/ArrowDown.svg" alt="down" />
+                  )}
+                </span>
+              </div>
+
+              {open2 && (
+                <ul className="absolute left-0 right-0 border border-[#C8C8C8] bg-white rounded-[3px] shadow-md z-10 max-h-48 overflow-y-auto">
+                  {optionservice
+                    .filter((opt) =>
+                      opt.toLowerCase().includes(searchValue2.toLowerCase())
+                    )
+                    .map((opt) => (
+                      <li
+                        key={opt}
+                        onClick={() => {
+                          setSelected2(opt);
+                          setOpen2(false);
+                          setSearchValue2("");
+                        }}
+                        className="p-3 hover:bg-[#F5F5F5] cursor-pointer"
+                      >
+                        {opt}
+                      </li>
+                    ))}
+                </ul>
+              )}
+
+            </div>
+          </div>          
         </div>
+      
+        {/* ========== status ========== */}
+        <div className="flex flex-col">
+          <label className="text-[#364152] text-base font-normal mb-3">
+            {t("Status")}
+          </label>
+
+          <div className="relative w-full" ref={dropdownRef3}>
+            <div
+              className="relative flex items-center border border-[#C8C8C8] rounded-[3px] cursor-pointer"
+              onClick={() => setOpen3(!open3)}
+            >
+              <input
+                type="text"
+                placeholder={t("Select status")}
+                value={selected3 || searchValue3}
+                onChange={(e) => {
+                  setSearchValue3(e.target.value);
+                  setOpen3(true);
+                  setSelected3(null);
+                }}
+                className="h-15 p-3 w-full text-[#364152] focus:outline-none"
+              />
+
+              <span className="absolute left-3 pointer-events-none">
+                {open3 ? (
+                  <img src="/images/icons/ArrowUp.svg" alt="up" />
+                ) : (
+                  <img src="/images/icons/ArrowDown.svg" alt="down" />
+                )}
+              </span>
+            </div>
+
+            {open3 && (
+              <ul className="absolute left-0 right-0 border border-[#C8C8C8] bg-white rounded-[3px] shadow-md z-10 max-h-48 overflow-y-auto">
+                {optionStatus
+                  .filter((opt) =>
+                    opt.toLowerCase().includes(searchValue3.toLowerCase())
+                  )
+                  .map((opt) => (
+                    <li
+                      key={opt}
+                      onClick={() => {
+                        setSelected3(opt);
+                        setOpen3(false);
+                        setSearchValue3("");
+                      }}
+                      className="p-3 hover:bg-[#F5F5F5] cursor-pointer"
+                    >
+                      {opt}
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
+        </div>
+
+
       </section>
 
+      
+      <section className="p-6 flex gap-4 ">
+        <button className="w-42.5 h-13.5 bg-[var(--color-primary)]  text-[#fff] rounded-[3px] text-base font-medium">
+          {t('Show results')}
+        </button>
 
+        <button className="w-35 h-13.5 border border-[var(--color-primary)]  text-[var(--color-primary)] rounded-[3px] text-base font-medium">
+          {t('Reset')}
+        </button>
+      </section>
 
       
     </Dialog>
