a8e2d44c5c2513b6dec980d5516e8ecb92ca30de
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2025-12-30 12:15:42 +0200

    feat(finance): conditionally render dropdown based on years count in income analysis chart

--- src/app/Pages/finance/Overview/Income_analysis/page.jsx
+++ src/app/Pages/finance/Overview/Income_analysis/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import TitleOfIncome_analysisPage from './TitleOfIncome_analysis/page'
 import ChartPage from './Chart/page'
+import { useDispatch } from 'react-redux';
 
 function Income_analysisPage() {
+  const dispatch  = useDispatch();
+   
   return (
     <>
     <div className='border border-[#E3E8EF] rounded-[3px] mb-12'>
