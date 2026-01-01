442ce2e19db5917134b7f9210e208357b9e9fdc2
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-01-01 15:27:32 +0200

    feat(settings): add additional pages for Basic Information, Your Files, Contact Information, Change Password, and Company Address to the settings menu

--- src/app/Pages/settings/SectionOfMenu/page.jsx
+++ src/app/Pages/settings/SectionOfMenu/page.jsx
@@ -4,6 +4,11 @@ import Company_dataPage from './Company_data/page'
 import Personal_dataPage from './Personal_data/page'
 import Marketer_PanelPage from './Marketer_Panel/page'
 import Activity_settingsPage from './Activity_settings/page'
+import BasicInformationPage from './Company_data/BasicInformation/page'
+import YourFilesPage from './Company_data/YourFiles/page'
+import ContactInformationPage from './Company_data/ContactInformation/page'
+import ChangePasswordPage from './Company_data/ChangePassword/page'
+import CompanyAddressPage from './Company_data/CompanyAddress/page'
 
 function SectionOfMenuPage({ selectedMenu }) {
   const renderContent = () => {
@@ -11,6 +16,26 @@ function SectionOfMenuPage({ selectedMenu }) {
       case 'Company_data':
         return (
           <Company_dataPage />
+        )
+      case 'BasicInformation':
+        return (
+          <BasicInformationPage />
+        )
+      case 'YourFiles':
+        return (
+          <YourFilesPage />
+        )
+      case 'ContactInformation':
+        return (
+          <ContactInformationPage />
+        )
+      case 'ChangePassword':
+        return (
+          <ChangePasswordPage />
+        )
+      case 'CompanyAddress':
+        return (
+          <CompanyAddressPage />
         )
       case 'Personal_data':
         return (
