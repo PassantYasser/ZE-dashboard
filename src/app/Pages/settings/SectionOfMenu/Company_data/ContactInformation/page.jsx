620095efc9c07ffb1565e4c9cad9a741742bb55b
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-01-05 11:10:58 +0200

    feat(settings): add contact information section to company data
    Implement contact information form with company number and WhatsApp number fields. Added new translations for both Arabic and English locales. Created dedicated Header component for the section.

--- src/app/Pages/settings/SectionOfMenu/Company_data/ContactInformation/page.jsx
+++ src/app/Pages/settings/SectionOfMenu/Company_data/ContactInformation/page.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+
+"use client";
+import React, { useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
+import Header from "./Header";
 
 function ContactInformationPage() {
+  const { t } = useTranslation();
+
+
+
   return (
-    <div>ContactInformationPage</div>
-  )
+    <div className="border border-[#E3E8EF]">
+      <Header />
+
+      <section className="p-6">
+        
+
+        {/* Company number */}
+        <div>
+          <p className="text-[#4B5565] text-base font-normal  mb-1.5">{t('Company number')}</p>
+          <input 
+            type="text"
+            placeholder='0000000000000'            
+            className="h-14 p-3 w-full rounded-[3px] border border-[#E3E8EF] shadow-xm outline-none placeholder:text-[#9A9A9A] placeholder:text-sm placeholder:font-normal" 
+          />
+        </div>
+
+        {/* WhatsApp number */}
+        <div className="mt-4"> 
+          <p className="text-[#4B5565] text-base font-normal  mb-1.5">{t('WhatsApp number')}</p>
+          <input 
+            type="text"
+            placeholder='0000000000000'            
+            className="h-14 p-3 w-full rounded-[3px] border border-[#E3E8EF] shadow-xm outline-none placeholder:text-[#9A9A9A] placeholder:text-sm placeholder:font-normal" 
+          />
+        </div>
+
+        
+      
+
+
+
+        {/* btn */}
+        <button className="bg-[var(--color-primary)] h-15 w-62.5 text-[#fff] text-base font-medium rounded-[3px] mt-6">
+          {t('Save changes')}
+        </button>
+      </section>
+
+      
+    </div>
+  );
 }
 
-export default ContactInformationPage
+export default ContactInformationPage;
