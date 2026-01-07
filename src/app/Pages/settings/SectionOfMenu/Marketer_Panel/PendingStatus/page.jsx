f0b38454cc3379c50ee1b5991546fb442513eb19
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-01-07 11:08:55 +0200

    feat(marketer-panel): enhance PendingStatusPage with localized messages and SVG image

--- src/app/Pages/settings/SectionOfMenu/Marketer_Panel/PendingStatus/page.jsx
+++ src/app/Pages/settings/SectionOfMenu/Marketer_Panel/PendingStatus/page.jsx
@@ -1,8 +1,16 @@
+import { t } from 'i18next'
 import React from 'react'
 
 function PendingStatusPage() {
   return (
-    <div>PendingStatusPage</div>
+    <>
+    <div className='flex flex-col justify-center items-center mb-9'>
+      <img src="/images/sandyHour.svg" alt="" className="w-25 h-25 mt-7.5" />
+      <p className='text-[#0F022E] text-lg font-semibold mt-6 mb-3'>{t('Application under review')} </p>
+      <p className='text-[#697586] text-sm font-normal'>{t('The data will be reviewed and you will be notified as soon as possible.')}</p>
+    </div>
+  
+    </>
   )
 }
 
