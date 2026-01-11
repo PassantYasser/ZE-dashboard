f7f0d4be5b4c6376eeea5c0308be4bf14ac45e03
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-01-11 15:50:43 +0200

    feat(personal-data): add PersonalDataPage and Header components with translation support

--- src/app/Pages/settings/SectionOfMenu/Personal_data/page.jsx
+++ src/app/Pages/settings/SectionOfMenu/Personal_data/page.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { use } from 'react'
+import Header from './Header'
+import { useTranslation } from 'react-i18next'
 
 function Personal_dataPage() {
+  const {t} = useTranslation()
   return (
-    <div>Personal_dataPage</div>
+    <>
+    <div className="border border-[#E3E8EF] mb-8">
+        <Header/>
+
+        <div className='grid grid-cols-3 gap-3 border border-[#E3E8EF] p-6'>
+          <div>
+            <p className='text-[#697586] text-base font-normal mb-1'>{t('First Name')}</p>
+            <p className='text-[#364152] text-sm font-normal'>حسام</p>
+          </div>
+
+          <div>
+            <p className='text-[#697586] text-base font-normal mb-1'>{t('Last Name')}/{t('Family Name')}</p>
+            <p className='text-[#364152] text-sm font-normal'>علاء</p>
+          </div>
+
+          
+
+          <div>
+            <p className='text-[#697586] text-base font-normal mb-1'>{t('National ID number')}</p>
+            <p className='text-[#364152] text-sm font-normal'>049731371857</p>
+          </div>
+
+          <div>
+            <p className='text-[#697586] text-base font-normal mb-1'>{t('Gender')}</p>
+            <p className='text-[#364152] text-sm font-normal'>ذكر</p>
+          </div>
+
+          <div>
+            <p className='text-[#697586] text-base font-normal mb-1'>{t('Nationality')}</p>
+            <p className='text-[#364152] text-sm font-normal'>مصري</p>
+          </div>
+        </div>
+    </div>
+
+    </>
   )
 }
 
