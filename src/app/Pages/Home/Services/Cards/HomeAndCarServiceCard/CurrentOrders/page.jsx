3376e49f96f2572e6d9efeb814b04bb286d2cde1
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-01-28 15:05:14 +0200

    feat(CurrentOrdersPage): improve status display and layout for better responsiveness

--- src/app/Pages/Home/Services/Cards/HomeAndCarServiceCard/CurrentOrders/page.jsx
+++ src/app/Pages/Home/Services/Cards/HomeAndCarServiceCard/CurrentOrders/page.jsx
@@ -8,55 +8,55 @@ function CurrentOrdersPage({ orders = [], layout = "list" }) {
     switch (status) {
       case "accepted": //تم القبول
         return (
-          <div className=' bg-[#DCFAE6] border border-[#067647] text-[#067647] w-fit  h-9.5 rounded-3xl'>
-          <div className='py-1.5 px-3 flex gap-1'>
-            <img src="/images/icons/Active Status.svg" alt="" className=' mt-1' />
-            <span className=''>{t('accepted')}</span>
+          <div className=' bg-[#DCFAE6] border border-[#067647] text-[#067647] w-fit  h-9.5 rounded-3xl flex justify-center items-center '>
+          <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+            <img src="/images/icons/Active Status.svg" alt="" className='w-4 h-4' />
+            <span className='text-xs lg1:text-sm'>{t('accepted')}</span>
           </div>
         </div>
         );
       case "completed"://مكتملة
         return (
-          <div className=' bg-[#DCFAE6] border border-[#067647] text-[#067647] w-fit  h-9.5 rounded-3xl'>
-          <div className='py-1.5 px-3 flex gap-1'>
-            <img src="/images/icons/Active Status.svg" alt="" className=' mt-1' />
-            <span className=''>{t('Complete')}</span>
+          <div className=' bg-[#DCFAE6] border border-[#067647] text-[#067647] w-fit  h-9.5 rounded-3xl flex justify-center items-center'>
+          <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+            <img src="/images/icons/Active Status.svg" alt="" className=' w-4 h-4 ' />
+            <span className='text-xs lg1:text-sm'>{t('Complete')}</span>
           </div>
         </div>
         );
       case "pending_approval": //في انتظار الموافقة
         return (
-          <div className=' bg-[#FFFAEB] border  border-[#F79009] text-[#DC6803] w-fit h-9.5 rounded-3xl'>
-            <div className='py-1.5 px-3 flex gap-1'>
-              <img src="/images/icons/pending Status.svg" alt=""className=' mt-1' />
-              <span className=''>{t('pending')}</span>
+          <div className=' bg-[#FFFAEB] border  border-[#F79009] text-[#DC6803] w-fit h-9 rounded-3xl flex justify-center items-center'>
+            <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+              <img src="/images/icons/pending Status.svg" alt=""className='w-4 h-4 ' />
+              <span className='text-xs lg1:text-sm'>{t('pending')}</span>
             </div>
           </div>
         );
       case "in_progress": //قيد التنفيذ
       return (
-        <div className=' bg-[#EFF4FF] border border-[#518BFF] text-[#004EEB] w-fit  h-9.5 rounded-3xl'>
-        <div className='py-1.5 px-3 flex gap-1'>
-          <img src="/images/icons/inactive Status.svg" alt="" className=' mt-1' />
-          <span className=''>{t('in_progress')}</span>
+        <div className=' bg-[#EFF4FF] border border-[#518BFF] text-[#004EEB] w-fit  h-9.5 rounded-3xl flex justify-center items-center'>
+        <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+          <img src="/images/icons/inactive Status.svg" alt="" className=' w-4 h-4 ' />
+          <span className='text-xs lg1:text-sm'>{t('in_progress')}</span>
         </div>
       </div>
       );
       case "on_going": //العامل في الطريق
         return (
-          <div className=' bg-[#E3E8EF] border border-[#697586] text-[#4B5565] w-fit h-9.5 rounded-3xl'>
-            <div className='py-1.5 px-3 flex gap-1'>
-              <img src="/images/icons/on_going Status.svg" alt="" className=' mt-1' />
-              <span className=''>{t('The worker on the road')}</span>
+          <div className=' bg-[#E3E8EF] border border-[#697586] text-[#4B5565] w-fit h-9.5 rounded-3xl flex justify-center items-center'>
+            <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+              <img src="/images/icons/on_going Status.svg" alt="" className=' w-4 h-4 ' />
+              <span className='text-xs lg1:text-sm'>{t('The worker on the road')}</span>
             </div>
           </div>
         );
       case "rejected": // مرفوضة
         return (
-          <div className=' bg-[#FEE4E2] border border-[#F97066] text-[#D92D20] w-fit h-9.5 rounded-3xl'>
-            <div className='py-1.5 px-3 flex gap-1'>
-              <img src="/images/icons/refused Status.svg" alt="" className=' mt-1'/>
-              <span className=''>{t('rejected')}</span>
+          <div className=' bg-[#FEE4E2] border border-[#F97066] text-[#D92D20] w-fit h-9.5 rounded-3xl flex justify-center items-center'>
+            <div className='lg1:py-1.5 lg1:px-3 py-1 px-2 flex gap-1'>
+              <img src="/images/icons/refused Status.svg" alt="" className=' w-4 h-4 ' />
+              <span className='text-xs lg1:text-sm'>{t('rejected')}</span>
             </div>
           </div>
         );
@@ -67,7 +67,7 @@ function CurrentOrdersPage({ orders = [], layout = "list" }) {
     <div className='border border-[#CDD5DF] rounded-[3px] p-6'>
       <p className='text-[#0F022E] text-xl font-medium'>{t('Current orders')}</p>
 
-      <div className={layout === "grid" ? "grid grid-cols-2 gap-4" : ""}>
+      <div className={layout === "grid" ? "grid grid-cols-2 gap-4" : `grid lg1:grid-cols-1 ${orders.length === 1 ? 'grid-cols-1' : 'grid-cols-2'} gap-4`}>
       {orders.map((order) => (
         <div key={order.id} className='mt-6 w-full border border-[#CDD5DF] bg-white shadow-sm rounded-[3px] p-4 mb-4'>
           <div className='flex justify-between w-full'>
@@ -80,8 +80,8 @@ function CurrentOrdersPage({ orders = [], layout = "list" }) {
 
           <hr className='border-[#E3E8EF] border my-4' />
 
-          <div className='flex justify-between w-full'>
-            <div className='flex gap-2 w-full'>
+          <div className='flex justify-between w-full '>
+            <div className='flex gap-2 lg1:w-full w-[70%]'>
               <img src='/images/icons/Customer.svg' alt='' className='w-6 h-6' />
               <p className='text-base font-normal'>
                 <span className='text-[#808080]'>{t('Customer')} </span>
@@ -89,7 +89,7 @@ function CurrentOrdersPage({ orders = [], layout = "list" }) {
               </p>
             </div>
 
-            <div className='w-full flex justify-end gap-4'>
+            <div className='flex justify-end gap-4 lg1:w-full w-[30%]'>
               <p className='rounded-[20px] w-7 h-7 border border-[#8B8B8B] flex items-center justify-center cursor-pointer'>
                 <img src='/images/icons/chat.svg' alt='' />
               </p>
