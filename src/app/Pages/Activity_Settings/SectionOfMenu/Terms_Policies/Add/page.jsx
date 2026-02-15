baccaf73a46ef2429acbdbf845e7e6c1ed185a96
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-02-15 10:23:54 +0200

    feat(terms-policies): add rich text editor for policy creation
    - Add TinyMCE React component as a rich text editor for the "Proposed text" field
    - Include new translation keys for "Proposed text" in both English and Arabic locales
    - Update package dependencies to include @tinymce/tinymce-react
    - Create basic form structure with address input field and disabled text editor
    - Configure editor with RTL support for Arabic language compatibility

--- src/app/Pages/Activity_Settings/SectionOfMenu/Terms_Policies/Add/page.jsx
+++ src/app/Pages/Activity_Settings/SectionOfMenu/Terms_Policies/Add/page.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import TextEditor from './TextEditor'
 
 function Addpage() {
+  const {t} = useTranslation()
   return (
-    <div>Addpage</div>
+    <>
+      <div className='p-6'>
+        {/*  */}
+        <div>
+          <p className='text-[#4B5565] text-sm font-normal'>{t('the address')}</p>
+          <input type="text"
+            className='w-full h-14 border border-[#E3E8EF] mt-1.5 rounded-[3px] outline-none px-4'  />
+        </div>
+        {/*  */}
+        <div className='mt-4'>
+          <p className='text-[#4B5565] text-sm font-normal'>{t('Proposed text')}</p>
+          <TextEditor  disabled/>
+        </div>
+
+      </div>
+
+
+    </>
   )
 }
 
