6716397192e1034bf27e98a4ee984cd484721d6d
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-02-19 15:41:41 +0200

    feat(services): dynamically render modules based on user data
    - Replace hardcoded module keys with dynamic retrieval from localStorage
    - Update condition checks to use English module keys for consistency
    - Add reusable NoModuleAvailable component for unavailable modules
    - Improve code structure by separating unavailable module display logic

---
+++ src/app/Components/DaialogsOfNavbar/NoModuleAvailable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../MainLayout/MainLayout'
+
+function NoModuleAvailable() {
+  return (
+    <MainLayout>
+      <div>المديول غير متاح</div>
+    </MainLayout>
+  )
+}
+
+export default NoModuleAvailable
--- src/app/Pages/Services/page.jsx
+++ src/app/Pages/Services/page.jsx
@@ -3,18 +3,21 @@ import MainLayout from '@/app/Components/MainLayout/MainLayout'
 import React from 'react'
 import Home_Car_Module from './Home_Car_Module/Service/page';
 import StreetAssistant_Module from './StreetAssistant_Module/Service/page';
+import NoModuleAvailable from '@/app/Components/DaialogsOfNavbar/NoModuleAvailable';
 
 
 function Servicespage() {
-  const current_module_key = 'خدمات المنازل'
-  let content;
+  const userData = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : null
+  const current_module_key = userData?.current_module_key
 
-  if (current_module_key === 'خدمات السيارات' || current_module_key === 'خدمات المنازل' ) {
+    let content;
+
+  if (current_module_key === 'car_services' || current_module_key === 'home_services' ) {
     content = <Home_Car_Module />;
-  } else if (current_module_key === 'خدمات الطريق') {
+  } else if (current_module_key === 'street_assistant') {
     content = <StreetAssistant_Module />;
   } else {
-    content = <div>المكون غير متاح</div>;
+    content = <NoModuleAvailable/>
   }
   return (
     <>
