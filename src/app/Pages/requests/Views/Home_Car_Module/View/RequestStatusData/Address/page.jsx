316dca14abfa616c1af9df1fa40537713a16f202
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-02-25 13:48:20 +0200

    feat: populate address details dynamically from booking data
    Replace hardcoded address values with dynamic data from the `bookingDetails.user_address` object. Each address field (e.g., street, apartment, city) is now conditionally rendered only if the corresponding data exists, improving data accuracy and user experience.

--- src/app/Pages/requests/Views/Home_Car_Module/View/RequestStatusData/Address/page.jsx
+++ src/app/Pages/requests/Views/Home_Car_Module/View/RequestStatusData/Address/page.jsx
@@ -6,110 +6,139 @@ function AddressPage({bookingDetails}) {
   const [open, setOpen] = useState(false);
 
   const {t}= useTranslation();
+  const AddressDetails = bookingDetails?.user_address
 
   return (
     <>
-  <div className="w-full p-3 shadow-[0_0_4px_0_rgba(0,0,0,0.3)] rounded-[3px] overflow-hidden bg-white select-none mt-6">
-      {/* Header */}
-      <button
-        onClick={() => setOpen(!open)}
-        className="w-full flex items-center justify-between   text-right"
-      >
-        <span className="text-[#0F022E] text-base font-normal flex items-center gap-2">
-          <img src="/images/icons/location2.svg" alt="" />
-          {t('Address details')}
-        </span>
-
-        <div
-          className={`transition-transform duration-300 cursor-pointer ${
-            open ? "rotate-180" : "rotate-0"
-          }`}
+    {AddressDetails && (
+      <div className="w-full p-3 shadow-[0_0_4px_0_rgba(0,0,0,0.3)] rounded-[3px] overflow-hidden bg-white select-none mt-6">
+        {/* Header */}
+        <button
+          onClick={() => setOpen(!open)}
+          className="w-full flex items-center justify-between   text-right"
         >
-          <img src="/images/icons/ArrowDown.svg" alt="" />
-        </div>
-      </button>
+          <span className="text-[#0F022E] text-base font-normal flex items-center gap-2">
+            <img src="/images/icons/location2.svg" alt="" />
+            {t('Address details')}
+          </span>
 
-      {/* Content */}
-      {open && (
-        <div className="px-3 mt-3  text-right ">
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] pt-2 pb-4">
-            <span className="text-[#575757] text-sm font-normal flex gap-2"> 
-              <img src='/images/icons/google-map-icon.svg'/>
-              <span>{t('the address')} :</span>
-            </span>
-            <span className="text-[#0F022E] text-sm font-medium">37 جمال الدين الماظة</span>
+          <div
+            className={`transition-transform duration-300 cursor-pointer ${
+              open ? "rotate-180" : "rotate-0"
+            }`}
+          >
+            <img src="/images/icons/ArrowDown.svg" alt="" />
           </div>
+        </button>
 
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Apartment number')}:</span>
-            <span className="text-[#0F022E]  font-medium">3</span>
+        {/* Content */}
+        {open && (
+          <div className="px-3 mt-3  text-right ">
+
+            {AddressDetails?.address_line && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] pt-2 pb-4">
+                <span className="text-[#575757] text-sm font-normal flex gap-2"> 
+                  <img src='/images/icons/google-map-icon.svg'/>
+                  <span>{t('the address')} :</span>
+                </span>
+                <span className="text-[#0F022E] text-sm font-medium">{AddressDetails?.address_line}</span>
+              </div>
+            )}
+
+            {AddressDetails?.apt_no && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Apartment number')}:</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.apt_no}</span>
+              </div>
+            )}
+
+            {AddressDetails?.floor && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('The role')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.floor}</span>
+              </div>
+            )}
+
+            {AddressDetails?.building_no && (
+              <div className="flex gap-0.5 text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Building name')} :</span>
+                <span className="text-[#0F022E]  font-medium"> {AddressDetails?.building_no}</span>
+              </div>
+            )}
+
+            {AddressDetails?.street_name && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Street name')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.street_name}</span>
+              </div>
+            )}
+
+            {AddressDetails?.po_box && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('P.O. Box number')} : </span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.po_box}</span>
+              </div>
+            )}
+
+            {AddressDetails?.nearest_sign && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Nearest landmark')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.nearest_sign}</span>
+              </div>
+            )}
+
+            {AddressDetails?.notes && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Additional instructions')}:</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.notes}</span>
+              </div>
+            )}
+
+            {AddressDetails?.neighborhood && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal"> {t('Neighborhood name')}:</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.neighborhood}</span>
+              </div>
+            )}
+
+            {AddressDetails?.address_class && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Title classification')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.address_class}</span>
+              </div>
+            )}
+
+            {AddressDetails?.address_type && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('Title type')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.address_type}</span>
+              </div>
+            )}
+
+            {AddressDetails?.city && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('City')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.city}</span>
+              </div>
+            )}
+
+            {AddressDetails?.state && (
+              <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
+                <span className="text-[#575757]  font-normal">{t('area')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.state}</span>
+              </div>
+            )}
+
+            {AddressDetails?.country && (
+              <div className="flex gap-0.5  text-sm pt-4">
+                <span className="text-[#575757]  font-normal">{t('State')} :</span>
+                <span className="text-[#0F022E]  font-medium">{AddressDetails?.country}</span>
+              </div>
+            )}
           </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('The role')} :</span>
-            <span className="text-[#0F022E]  font-medium">5</span>
-          </div>
-
-          <div className="flex gap-0.5 text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Building name')} :</span>
-            <span className="text-[#0F022E]  font-medium">عمارة المرور</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Street name')} :</span>
-            <span className="text-[#0F022E]  font-medium">صلاح</span>
-          </div>
-
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('P.O. Box number')} : </span>
-            <span className="text-[#0F022E]  font-medium">صلاح</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Nearest landmark')} :</span>
-            <span className="text-[#0F022E]  font-medium">جنينه</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Additional instructions')}:</span>
-            <span className="text-[#0F022E]  font-medium">-</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal"> {t('Neighborhood name')}:</span>
-            <span className="text-[#0F022E]  font-medium">-</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Title classification')} :</span>
-            <span className="text-[#0F022E]  font-medium">سكن</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('Title type')} :</span>
-            <span className="text-[#0F022E]  font-medium">شقة</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('City')} :</span>
-            <span className="text-[#0F022E]  font-medium">مدينة نصر</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm border-b border-[#E3E8EF] py-4">
-            <span className="text-[#575757]  font-normal">{t('area')} :</span>
-            <span className="text-[#0F022E]  font-medium">القاهرة</span>
-          </div>
-
-          <div className="flex gap-0.5  text-sm pt-4">
-            <span className="text-[#575757]  font-normal">{t('State')} :</span>
-            <span className="text-[#0F022E]  font-medium">مصر</span>
-          </div>
-
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    )}
     </>
   )
 }
