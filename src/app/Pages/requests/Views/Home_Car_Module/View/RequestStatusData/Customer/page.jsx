0464f9fca01424a7f8dbe64a95f2647b03024195
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-02-26 11:30:32 +0200

    make conditional rendering of user details in CustomerPage

--- src/app/Pages/requests/Views/Home_Car_Module/View/RequestStatusData/Customer/page.jsx
+++ src/app/Pages/requests/Views/Home_Car_Module/View/RequestStatusData/Customer/page.jsx
@@ -4,42 +4,44 @@ import { useTranslation } from 'react-i18next';
 
 function CustomerPage({bookingDetails}) {
   const { t } = useTranslation();
+  const userDetails = bookingDetails?.user;
   return (
     <>
-      <section className='flex justify-between shadow-[0_0_4px_0_rgba(0,0,0,0.3)] rounded-[3px] p-4 mt-6'>
+      {userDetails &&  (
+        <section className='flex justify-between shadow-[0_0_4px_0_rgba(0,0,0,0.3)] rounded-[3px] p-4 mt-6'>
 
-        <div className='flex gap-3 '>
-          {/* image   */}
-          <div className=' flex items-center'>
-            <div className='bg-[#E3E8EF] w-10 h-10 flex items-center justify-center rounded-[999px]'>
-              <img src="/images/icons/Customer.svg" alt="" />
+          <div className='flex gap-3 '>
+            {/* image   */}
+            <div className=' flex items-center'>
+              <div className='bg-[#E3E8EF] w-10 h-10 flex items-center justify-center rounded-[999px]'>
+                <img src="/images/icons/Customer.svg" alt="" />
+              </div>
+            </div>
+            {/* Name */}
+            <div className='text-sm font-normal'>
+              <p className='text-[#808080] '>{t('Customer')}</p>
+              <p className='text-[#0B2C3E] mt-1 '>{bookingDetails?.user?.name} {bookingDetails?.user?.lastname}</p>
             </div>
           </div>
-          {/* Name */}
-          <div className='text-sm font-normal'>
-            <p className='text-[#808080] '>{t('Customer')}</p>
-            <p className='text-[#0B2C3E] mt-1 '>{bookingDetails?.user?.name} {bookingDetails?.user?.lastname}</p>
+
+          <div className='flex items-center gap-4'>
+
+            {/* chat */}
+            <div className='border border-[#946DF1] w-10 h-10 flex justify-center items-center rounded-[999px]'>
+              <img src="/images/icons/chatIcon.svg" alt="" />
+            </div>
+
+            {/* call */}
+            <a
+              href={`tel:${bookingDetails?.user?.phone}`}
+              className='border border-[#946DF1] w-10 h-10 flex justify-center items-center rounded-[999px]'>
+              <img src="/images/icons/call.svg" alt="" />
+            </a>
+
           </div>
-        </div>
-
-        <div className='flex items-center gap-4'>
-
-          {/* chat */}
-          <div className='border border-[#946DF1] w-10 h-10 flex justify-center items-center rounded-[999px]'>
-            <img src="/images/icons/chatIcon.svg" alt="" />
-          </div>
-
-          {/* call */}
-          <a
-            href={`tel:${bookingDetails?.user?.phone}`}
-            className='border border-[#946DF1] w-10 h-10 flex justify-center items-center rounded-[999px]'>
-            <img src="/images/icons/call.svg" alt="" />
-          </a>
-
-        </div>
-      
-      </section>
-
+        
+        </section>
+      )}
     </>
   )
 }
