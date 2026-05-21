dade12b6beba5f4eeed09daa88ab9eadcbca447f
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-05-21 10:57:51 +0300

    feat(queue): add new queue module page and assets
    add "Coming" and "Late" translation strings to en/ar locales
    create queue stats dashboard page
    update sidebar link to new queue module route
    add required calendar status icons

---
+++ src/app/Pages/requests/Queue_Module/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import MainLayout from '@/app/Components/MainLayout/MainLayout'
+import React from 'react'
+import BoxPage from './Box/page'
+
+function Queue_ModulePage() {
+  return (
+    <MainLayout>
+      <BoxPage/>
+    </MainLayout>
+  )
+}
+
+export default Queue_ModulePage
