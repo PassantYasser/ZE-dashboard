505dff92b17287751339395717bdb12cf510e1dc
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-06-17 09:23:57 +0300

    feat(pending-list): add pending list page and update translations

---
+++ src/app/Pages/Pending_List/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import MainLayout from '../../Components/MainLayout/MainLayout'
+
+function Pending_ListPage() {
+  return (
+    <MainLayout>Pending_ListPage</MainLayout>
+  )
+}
+
+export default Pending_ListPage
