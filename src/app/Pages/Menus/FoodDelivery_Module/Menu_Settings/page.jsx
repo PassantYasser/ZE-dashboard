aa18365ada026ba4510201f7389841e16763a661
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-07-30 11:02:03 +0300

    feat(MenuSettings): add Menu_Settings page and update routing in AddPage and Header components

---
+++ src/app/Pages/Menus/FoodDelivery_Module/Menu_Settings/page.jsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/app/Components/MainLayout/MainLayout'
+import React from 'react'
+
+function Menu_SettingsPage() {
+  return (
+    <MainLayout>Menu_SettingsPage</MainLayout>
+  )
+}
+
+export default Menu_SettingsPage
