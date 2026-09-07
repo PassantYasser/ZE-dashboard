9a34c50a9de33ebfa197454926198d833b6a9ab5
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-09-07 14:34:40 +0300

    Add Card and NoCards components; enhance Freelance page with empty state handling and translations

--- src/app/Pages/Home/Delivery_Module/Freelance/page.jsx
+++ src/app/Pages/Home/Delivery_Module/Freelance/page.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BoxPage from './Box/page'
 import TitlePage from './Title/page'
+import Card from './Cards/Card'
+import NoCards from './Cards/NoCards'
 
 function FreelancePage() {
   return (
     <div>
       <TitlePage/>
       <BoxPage/>
+      {/* <Card/> */}
+      <NoCards/>
     </div>
   )
 }
