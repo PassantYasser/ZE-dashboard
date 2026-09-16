b229be5e15cd42150e97de43909b5ab51394a8f2
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2026-09-16 10:08:48 +0300

    feat(delivery): implement CompanyPage with animated delivery status cards and filter functionality; add localization for delivery labels

---
+++ src/app/Pages/Connections/Delivery_Module/Company/Details/page.jsx
@@ -0,0 +1,12 @@
+import MainLayout from '@/app/Components/MainLayout/MainLayout'
+import React from 'react'
+
+function DetailsPage() {
+  return (
+    <MainLayout>
+        <div>DetailsPage</div>
+    </MainLayout>
+  )
+}
+
+export default DetailsPage
